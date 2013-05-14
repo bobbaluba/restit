ee542866eef7533f0a271c225652d85eaf2ad2c8
Johan Klokkhammer Helsing <bobbaluba@gmail.com>
2013-05-14 20:16:42 +1000

    Trying to adopt the solution from zucker & maas

--- src/stochyboss.cpp
+++ src/stochyboss.cpp
@@ -3,7 +3,8 @@
 #include <vector>
 #include <limits>
 
-StochyBoss::StochyBoss() {
+
+StochyBoss::StochyBoss(float learningRate) : learningRate(learningRate){
 }
 
 BorisGoal StochyBoss::getGoal(const State &currentState){
@@ -17,11 +18,7 @@ BorisGoal StochyBoss::getGoal(const State &currentState){
         std::vector<int> features = getFeatures(currentState, action);
 
         //create parameter vector
-        std::vector<float> parameterVector(features.size(), 0);
-
-        //set weight for numlinesremoved and number of holes
-        parameterVector[features.size()-1] = 1; //linesremoved
-        parameterVector[features.size()-2] = -1; //number of holes
+        std::vector<float> parameterVector = getParameterVector(features.size());
 
         float score = calculateQuality(parameterVector, features);
 
@@ -33,6 +30,12 @@ BorisGoal StochyBoss::getGoal(const State &currentState){
     return bestAction;
 }
 
+void StochyBoss::updateWeights(const std::vector<float> &delta){
+    for(unsigned int i=0; i<parameterVector.size(); ++i){
+        parameterVector[i] += learningRate*delta[i];
+    }
+}
+
 float StochyBoss::calculateQuality(const std::vector<float> parameterVector, const std::vector<int> features){
     //evaluate state
     float score = 0;
@@ -40,6 +43,11 @@ float StochyBoss::calculateQuality(const std::vector<float> parameterVector, con
         score += parameterVector[i] * features[i];
     }
     return score;
+}
+
+float StochyBoss::z(){
+    const float discount = 0.7f;
+
 }
 
 
@@ -50,4 +58,19 @@ std::vector<int> StochyBoss::getFeatures(const State& currentState, const BorisG
     std::vector<int> features = nextBoard.getFeatures();
     features.push_back(linesRemoved);
     return features;
+}
+
+std::vector<float> StochyBoss::getParameterVector(int size){
+    if(parameterVector.empty()){
+        //create parameter vector
+        parameterVector.reserve(size);
+        for(int i = 0; i<size; ++i){
+            parameterVector.push_back(-1 + (float)rand()/((float)RAND_MAX/(1+1)));
+        }
+        //debug for a vector that performs reasonably well
+        //set weight for numlinesremoved and number of holes
+        //parameterVector[22-1] = 1; //linesremoved
+        //parameterVector[22-2] = -1; //number of holes
+    }
+    return parameterVector;
 }
--- src/stochyboss.h
+++ src/stochyboss.h
@@ -4,13 +4,22 @@
 #include "bossofboris.h"
 
 class StochyBoss : public BossOfBoris {
+private:
+    std::vector<float> parameterVector;
+    std::vector<float> delta;
+    const float learningRate;
 public:
-    StochyBoss();
+    StochyBoss(float learningRate);
     virtual BorisGoal getGoal(const State &currentState);
+    void updateWeights(const std::vector<float> &delta);
 private:
     float evaluateAction(BorisGoal action);
     std::vector<int> getFeatures(const State& currentState, const BorisGoal& action);
+    std::vector<float> getParameterVector(int size);
     float calculateQuality(const std::vector<float> parameterVector, const std::vector<int> features);
+
+    //from zucker & maas
+    float z();
 };
 
 #endif // STOCHYBOSS_H
