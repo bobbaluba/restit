34d0d87d841132795c8d27e4c25edd4af85fcd2e
Johan Klokkhammer Helsing <bobbaluba@gmail.com>
2013-06-08 15:17:38 +1000

    Simplified state so it's just a wrapper for gamemodel.

--- src/state.cpp
+++ src/state.cpp
@@ -1,11 +1,3 @@
 #include "state.h"
 
-const std::vector<SimpleAction> State::getLegalActions() const{
-    return board.getLegalActions(currentPiece);
-}
-
-const BoardModel State::applyAction(const SimpleAction &action, int *numLinesRemoved) const {
-    return board.applyAction(action, currentPiece, numLinesRemoved);
-}
-
 
